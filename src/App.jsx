ca8c3688b6eeaf145b03b5aba996690f977d2634
hanbanshen <2993999835@qq.com>
2026-05-27 18:32:17 +0800

    初始化 React 前端项目

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Node 商城前端</h1>
+
+      <p>这是我的第一个 React 页面。</p>
+
+      <div className="card">
+        <h2>当前学习目标</h2>
+        <p>先学会 React 页面结构，再学习调用后端接口。</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
